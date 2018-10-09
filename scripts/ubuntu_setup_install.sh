f43725dec9fab27e29c0ac2da6cb0a6e3063723f
Matthias Uttendorfer <matthias.uttendorfer@audi.de>
2018-10-09 11:11:56 +0200

    Remove sudo from script

--- scripts/ubuntu_setup_install.sh
+++ scripts/ubuntu_setup_install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/ bash
 
-sudo apt-get update
+apt-get update
 
 # WGet
-sudo apt-get install wget
+apt-get install wget
 
 # CMake
 wget https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.sh
 chmod +x cmake-3.12.3-Linux-x86_64.sh
-sudo ./cmake-3.12.3-Linux-x86_64.sh
+./cmake-3.12.3-Linux-x86_64.sh
 
 # Visual Studio Code
 wget https://go.microsoft.com/fwlink/?LinkID=760868
-sudo dpkg -i code_1.28.0-1538751525_amd64.deb
+dpkg -i code_1.28.0-1538751525_amd64.deb
 
 # Install Clang
 
@@ -20,18 +20,18 @@ sudo dpkg -i code_1.28.0-1538751525_amd64.deb
 CLANG_VERSION=6.0
 
 echo '--> uninstall other versions of clang compiler & tools...'
-sudo apt-get remove clang* -y
+apt-get remove clang* -y
 
 echo '--> install Clang compiler and tools...'
-sudo apt-get install -y \
+apt-get install -y \
         clang-${CLANG_VERSION} \
         clang-tidy-${CLANG_VERSION} \
         clang-format-${CLANG_VERSION}
 
 echo '--> make clang the default compiler and set aliases for clang tools...'
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-${CLANG_VERSION} 100
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-${CLANG_VERSION} 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} 100
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
-sudo update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-${CLANG_VERSION} 100
-sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-${CLANG_VERSION} 100
+update-alternatives --install /usr/bin/cc cc /usr/bin/clang-${CLANG_VERSION} 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-${CLANG_VERSION} 100
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} 100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} 100
+update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-${CLANG_VERSION} 100
+update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-${CLANG_VERSION} 100
