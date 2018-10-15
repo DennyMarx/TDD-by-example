d7ff96596f0859bfeabb8cd704d0c412708202b1
Matthias Uttendorfer <matthias.uttendorfer@audi.de>
2018-10-15 14:42:00 +0200

    Remove solution for examples
    solution only on master

--- fibonacci/src/fibonacci.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <cstdlib>
-
-int fibonacci(int input);
--- fusion/src/Fusion.hpp
+++ fusion/src/Fusion.hpp
@@ -1,19 +1,10 @@
 #pragma once
 
 #include <DistanceSensor.hpp>
-#include <functional>
-#include <memory>
-#include <vector>
 
 class Fusion
 {
 public:
-    Fusion(const std::vector<DistanceSensor*>& distance_sensors,
-           std::function<double()> velocity_sensor);
-
-    double calculateTimeToCollision();
 
 private:
-    std::vector<DistanceSensor*> _distance_sensors;
-    std::function<double()> _velocity_sensor;
 };
