59bf021aa8a5134879511d440eca9c7b53074d56
Matthias Uttendorfer <matthias.uttendorfer@audi.de>
2018-10-15 14:42:00 +0200

    Remove solution for examples
    solution only on master

--- fibonacci/test/fibonacciTests.cpp
+++ fibonacci/test/fibonacciTests.cpp
@@ -1,12 +1,1 @@
 
-#include <GUnit.h>
-#include <fibonacci.hpp>
-
-GTEST("fibonacci testing")
-{
-    EXPECT_EQ(fibonacci(1), 1);
-    EXPECT_EQ(fibonacci(2), 1);
-    EXPECT_EQ(fibonacci(3), 2);
-    EXPECT_EQ(fibonacci(4), 3);
-    EXPECT_EQ(fibonacci(21), 10946);
-}
