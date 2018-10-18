6dd5bd88d1c34a90464d70d7e9bce70a59f630a6
Matthias Uttendorfer <matthias.uttendorfer@audi.de>
2018-10-18 14:46:50 +0200

    Edit test tasks

--- fusion/test/FusionTest.cpp
+++ fusion/test/FusionTest.cpp
@@ -12,7 +12,7 @@ GTEST("Testing Time to Collision")
     std::vector<DistanceSensor*> distance_sensors = {
         &distance_sensor_1.object(), &distance_sensor_2.object(), &distance_sensor_3.object()};
 
-    SHOULD("Return TTC = 0.02s when closest distance measurement is 5.0m with velocity of 50 m/s")
+    SHOULD("Return TTC = 0.5s when closest distance measurement is 5.0m with velocity of 50 m/s")
     {
         std::function<double()> velocity_sensor = []() { return 10; };
 
@@ -30,16 +30,11 @@ GTEST("Testing Time to Collision")
     SHOULD("Return TTC = 1.0s when closest plausible (certainty > 70 % ) distance measurement is "
            "10.0m with velocity of 50 m/s")
     {
-        std::function<double()> velocity_sensor = []() { return 10; };
+        EXPECT_FALSE(true);
+    }
 
-        EXPECT_CALL(distance_sensor_1, (getDistanceToClosestObject)())
-            .WillOnce(Return(DistanceSensor::DistanceMeasurement{12.0, 0.9}));
-        EXPECT_CALL(distance_sensor_2, (getDistanceToClosestObject)())
-            .WillOnce(Return(DistanceSensor::DistanceMeasurement{10.0, 0.9}));
-        EXPECT_CALL(distance_sensor_3, (getDistanceToClosestObject)())
-            .WillOnce(Return(DistanceSensor::DistanceMeasurement{5.0, 0.4}));
-
-        auto fusion = Fusion(distance_sensors, velocity_sensor);
-        EXPECT_EQ(1.0, fusion.calculateTimeToCollision());
+    SHOULD("Return ??? when velocity < 0")
+    {
+        EXPECT_FALSE(true);
     }
 }
