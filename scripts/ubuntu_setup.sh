de2963f7b2cb5d4b0588ad048d4a2f184238945d
defonzie <43653796+defonzie@users.noreply.github.com>
2018-10-18 16:00:00 +0200

    Update setup script

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -11,17 +11,32 @@ sudo apt-get install -y \
     git \
     build-essential
 
+git --version
+gcc --version
+g++ --version
+make --version
+
 # CMake
 wget -qO- "https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.tar.gz" | sudo tar --strip-components=1 -xz -C /usr/local
+
+cmake --version
 
 # Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+rm -f microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get install -y code
+
+code --version
 
 code --install-extension eamodio.gitlens
 code --install-extension ms-vscode.cpptools
 code --install-extension twxs.cmake
 code --install-extension vector-of-bool.cmake-tools
+
+# clone repo
+pushd $HOME
+git clone https://github.com/MatthiasU/TDD-by-example.git
+popd
